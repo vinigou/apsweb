b72c877d7a30db5dcdd9d662190454df3f4071a1
Vinícius Gouveia da Silva <133818471+vinigou@users.noreply.github.com>
2023-05-23 23:11:55 -0300

    Update script-login.js

--- assets/js/script-login.js
+++ assets/js/script-login.js
@@ -1,3 +1,5 @@
+//SCRIPT FUNÇÕES DO LOGIN
+
 function alertaEntrar() {
     var desc = document.getElementById('login-box');
     if (window.getComputedStyle(desc).display === "none") {
@@ -10,6 +12,19 @@ function Entrar() {
     document.getElementById('bem-vindo').classList.add('hidden');
     document.getElementById('necessario-entrar').classList.add('hidden');
     document.getElementById('login-box').classList.remove('hidden');
+    document.getElementById('login-section').classList.remove('hidden');
+
+    var btn = document.querySelector('#show-login');
+    var container = document.querySelector('section');
+
+    btn.addEventListener('click', function(){
+        if(container.style.display === 'flex'){
+            container.style.display = 'nome';
+        } else{
+            container.style.display = 'flex';
+        }
+
+    });
 }
 
 const form = document.getElementById('login-form');
